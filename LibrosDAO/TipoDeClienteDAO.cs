770dc5c97e347cf1e7bf223551714c4f81a79d63
Cristian Casco <ccasco@outlook.es>
2022-06-28 15:37:26 -0600

    Cambios 280622

---
+++ LibrosDAO/TipoDeClienteDAO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using LibrosEDM;
+using LibrosEDM.ModeloAplicacion;
+
+namespace LibrosDAO
+{
+    public class TipoDeClienteDAO
+    {
+        LibrosModelConatiner db = new LibrosModelConatiner();   
+
+        public bool Agregar(TipoCliente oTipo)
+        {
+            db.TipoCliente.add(oTipo);
+            return (db.SaveChanges) > 0 ? true: false ;
+        }
+
+        public bool Modificar(TipoCliente oTipo)
+        {
+            db.Entry(oTipo).State = EntityState.Modified;
+            return (db.SaveChanges) > 0 ? true : false;
+        }
+
+        public bool Eliminar(TipoCliente oTipo)
+        {
+            db.TipoCliente.Remove(oTipo);
+            return (db.SaveChanges) > 0 ? true : false;
+        }
+
+        public TipoCliente Buscar(string pCodigo)
+        {
+            TipoCliente oTipo;
+            oTipo = db.TipoClinte.DefaultIfEmpy(null).FirstOrDefault(tc => tc.CodigoTipoCliente.Trim() == pCodigo.Trim());
+            return (oTipo);
+        }
+
+    }
+}
